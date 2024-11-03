4f9d3678ba3eb9e943d208a1969a29f9547e606f
Michael Hosamani <michaelhosamani26@gmail.com>
2024-11-04 01:07:58 +0530

    added calendar icon in ScheduleCard component

--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -3,8 +3,12 @@ export function ScheduleCard({time, status, title}) {
     
     return <div className="w-[29rem] overflow-hidden bg-white dark:bg-zinc-950 h-[24rem] shadow-[0px_5px_70px_1px_rgba(0,0,0,0.1)] rounded-2xl flex flex-col  ">
         <div className="h-10 bg-gray-100 dark:bg-gray-300 m-4 rounded-lg ">
-            {/* Calendar */}
-           <p className="text-center mt-2"> Monday, 14 oct 2024  </p>
+            {/* Calendar */}    
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mt-2 ml-7">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+            </svg>
+
+           <p className="text-center mt-[-1.5rem]"> Monday, 14 oct 2024  </p>
         </div>
         <div className="flex gap-4 ml-5 ">
             <div>
